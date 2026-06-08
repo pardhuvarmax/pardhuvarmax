8839fd63a74ae521a87cb05f9a8f5ececb9f4800
PardhuVarma Konduru <pardhuvarma.kernel@gmail.com>
2026-06-08 22:30:24 +0530

    fix(lbsm): profile update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import "./styles/index.css";
-import { MusicProvider } from "./context/MusicContext";
+import { MusicProvider } from "./context/MusicContext.js";
 
 createRoot(document.getElementById("root")!).render(
   <MusicProvider>
